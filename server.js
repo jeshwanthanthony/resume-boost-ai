1fc2d4053cd327571a7b1e87d2de015a88abedd6
jeshanthony <janthon8@gmu.edu>
2025-05-12 22:32:55 -0400

    Use Render's dynamic port

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const fs = require('fs');
 const multer = require('multer');
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 
 // Middleware
 app.use(express.static('public'));
